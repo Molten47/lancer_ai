989d631c0862a5c8dc5e9f3b4c9e2d250f90c7cb
Alawiye Sodeeq <alawiyesodeq@gmail.com>
2025-05-08 00:28:20 -0400

    update made to vteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/new-form',
+  base:'/new-form/',
   plugins: [react(), tailwindcss()],
 })
